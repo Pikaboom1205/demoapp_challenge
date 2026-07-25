ca2fc6a8018b35271a88f56b6e3e9bda18f96648
Chen Guanzi <jcg120512@gmail.com>
2026-07-25 11:14:52 +0800

    Initial Commit

---
+++ demoapp_challenge2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demoapp_challenge2
+//
+//  Created by JC on 25/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demoapp_challenge2/demoapp_challenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  demoapp_challenge2App.swift
+//  demoapp_challenge2
+//
+//  Created by JC on 25/7/26.
+//
+
+import SwiftUI
+
+@main
+struct demoapp_challenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
